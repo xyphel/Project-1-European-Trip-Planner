d411f7a242ccf129026af4ce391e36552cef40c9
cree321 <42984414+cree321@users.noreply.github.com>
2022-09-21 16:36:50 -0700

    Update index.js
    See https://docs.google.com/document/d/1WKuNj9bUIIz6kf06DeVW9oiCay7lb15iArRUkUG3f78/edit?usp=sharing for history.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
+// index.js
 // Code
+
+// https://javascript.info/keys-values-entries
+function testPrint(inputMap)
+{
+  for( let startCity of Object.keys(inputMap) )
+  {
+    for( let endCity of Object.entries(startCity) )
+    {
+      console.log(”From %s to %s is %i miles.”, startCity, endCity[0], endCity[1]);
+    }
+  }
+}
+
+function main()
+{
+  const distanceRequest = new Request(“distances.json”);
+  var cityDistances;
+  
+  // fetch returns a promise, which   must be handled, in case file is not found
+  fetch(distanceRequest).then(
+
+  // the contents of the file may not be valid JSON, so another promise must be handled
+  (response) => response.json()).then(
+
+    // what to do with the city distances object once received
+  (distancesObj) =>
+    cityDistances = distancesObj
+
+  // catching possible exceptions
+  ).catch(console.error);
+
+testPrint(cityDistances);
+}
+// extending - file & storage apis
