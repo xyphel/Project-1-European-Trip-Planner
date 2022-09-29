0df213b9bf9d1c62fac2f252990829b194119ac6
andrewcesario1 <63276479+andrewcesario1@users.noreply.github.com>
2022-09-29 16:30:32 -0700

    Changed window titles

--- QT/mainwindow.cpp
+++ QT/mainwindow.cpp
@@ -15,9 +15,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-
-
 void MainWindow::on_pushButton_clicked()
 {
     db.open();
--- QT/mainwindow.h
+++ QT/mainwindow.h
@@ -27,7 +27,6 @@ private slots:
 
     void on_pushButton_3_clicked();
 
-
 private:
     Ui::MainWindow *ui;
     planTripWindow *tripWindow;
