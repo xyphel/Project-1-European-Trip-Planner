fca8f23f07566bc886cf8f9b7cc73adff7e36eff
Trazler McGrath <trazmcg@gmail.com>
2022-10-11 01:28:18 -0700

    Update TravelPlan Object. Bugfixing and integrating to come.

--- QT/travelplan.h
+++ QT/travelplan.h
@@ -1,11 +1,11 @@
-//#ifndef TRAVELPLAN_H
-//#define TRAVELPLAN_H
+#ifndef TRAVELPLAN_H
+#define TRAVELPLAN_H
 
-//#include <QtSql/QSqlDatabase>
-//#include <vector>
-//#include <string>
+#include <QtSql/QSqlDatabase>
+#include <vector>
+#include <QString>
 
-//using namespace std;
+using namespace std;
 
 //struct Location
 //{
@@ -14,33 +14,62 @@
 //};
 
 
-//class TravelPlan
-//{
-//protected:
-//    // currentLocation is composed of
-//    //  1. cityName: string of city name
-//    //  2. distance: total number of km traveled
-//    Location currentLocation;
-//    vector<string> destinations;
-//public:
-//    // Construct & Destruct
-//    TravelPlan();
-//    ~TravelPlan();
-//    // Read-only properties
-//    string currentCity();
+
+class TravelPlan
+{
+private:
+    /*! func Compare
+     * brief Returns whether two QStrings are equal.
+     */
+    bool Compare(const QString& str1, const QString& str2);
+protected:
+
+    vector<string> visitedCities; /*!< Contains previosly visited cities' names. */
+    QSqlDatabase& db; /*!< Poimter to primary, shared sql database. */
+    int cityIndex; /*!< Index of current city. */
+    int cityCount; /*!< Number of cities in this plan. */
+public:
+    /*! struct Receipt
+     * brief Container for receipt-specific data members.
+     */
+    struct Receipt
+    {
+        std::vector<double> costOfItems; /*!< Cost of each purchased item. Order synced with itemsBought */
+        std::vector<QString> itemsBought; /*!< Name of each purchased item. Order synced with costOfItems */
+        double cost; /*!< Total travel expenses. */
+        int distanceTraveled; /*!< Total distance traveled. */
+    };
+    QString currentCity; /*!< Name of current city. */
+    Receipt receipt; /*!< This plan's receipt. */
+
+    /*! func TravelPlan
+     * brief Constructor requires a QSqlDatabase reference.
+     */
+    TravelPlan(QSqlDatabase& db);
+    /*! func ~TravelPlan
+     * brief Destructor.
+     */
+    ~TravelPlan();
+    /*! func FindClosestCity
+     * brief Constructor requires a QSqlDatabase reference.
+     */
+    void FindClosestCity(const QString& CITY, std::vector<QString>& cities);
+    bool CheckIfCityWasVisited(const QString& CITY, std::vector<QString>& visitedCities);
+
+
+
+    // Read-only properties
 //    int distanceTraveled();
 //    // Mutators include a recursive sort
-//<<<<<<< HEAD
+
 //    void addDestination(string city);
 //    void removeDestination(string city);
-//    //void GetDatabase(); ???
-//=======
-//>>>>>>> e960ce450a589a15eab2be34385a25753824025e
+    //void GetDatabase(); ???
 
-//    // TravelPlan already owns detinations, so only
-//    // SQL database handle is needed.
-//    Location findNearestDestination(QSqlDatabase* db);
-//    vector<Location> findQuickestRoute(vector<string> destinations, QSqlDatabase* db);
-//};
+    // TravelPlan already owns detinations, so only
+    // SQL database handle is needed.
+    //Location findNearestDestination(QSqlDatabase* db);
+    //vector<Location> findQuickestRoute(vector<string> destinations, QSqlDatabase* db);
+};
 
-//#endif // TRAVELPLAN_H
+#endif // TRAVELPLAN_H
